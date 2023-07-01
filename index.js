1accdac4cb3ed6e91570f48bcba1f9563dd0a2d3
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-07-01 17:57:05 +0300

    PROJECT1 Getting side of hypotenuse triangle

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-//Math Object
+let a;
+let b;
+let c;
 
-//round off to the nearest integer
-let x = 3.14
- x = Math.round(x)
- console.log(x) //3
+a = window.prompt("Enter side A")
+b = window.prompt("Enter side B")
+//convert to number type
+a = Number(a)
+b = Number(b)
 
- //round up Math.ceil()
- let y = 3.67
- y = Math.ceil(y)
- console.log(y) //4
+c = Math.pow(a, 2) + Math.pow(b, 2)
+c = Math.sqrt(c)
+console.log(c)
 
- //round down Math.floor()
- let z = 3.99
- z = Math.floor(z)
- console.log(z) //3
 
- //raise a number to a given power Math.pow()
- let n = 5
- n = Math.pow(n, 2)
- console.log(n) //25
+//FROM HTML
 
- //square root of a value
- let root = Math.sqrt(n)
- console.log(root) //5
+document.getElementById('submitButton').onclick = function(){
+    let aa = document.getElementById('aText').value;
+    aa = Number(aa)
 
- //absolute value
- let num = -338;
- let abs = Math.abs(num)
- console.log(abs) //338
+    let bb = document.getElementById('bText').value;
+    bb = Number(bb)
 
- //Minimum values Math.min()
- //Maximum values Math.max()
+    let cc = Math.sqrt(Math.pow(aa, 2) + Math.pow(bb, 2))
 
- let i = 48.3
- let j = 88
- let k = 39.22
-
- let min = Math.min(i, j, k)
- console.log("Min Value: ", min) //39.22
-
- let max = Math.max(i,j,k)
- console.log("Max Value: ", max) //88
+    document.getElementById('cLabel').innerHTML = 'Side C: ' + cc
+}
